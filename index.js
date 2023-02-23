c2cf43b37de54366bab67d05ceb44cbf4c7e7064
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-02-23 12:47:13 -0300

    lectures 65 and 66: understanding the concept of routes and URLS, package.Json and installing express.

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const http = require('http')
 
-http.createServer((res, res) => {
+let server = http.createServer((req, res) => {
 
     console.log('URL', req.url)
     console.log('METHOD', req.method)
+
+    switch (req.url) {
+
+        case '/':
+
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/html')
+            res.end('<h1>Olá</h1>')
+    
+        break
+
+        case '/users':
+
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify({
+                users: [{
+                    name: 'Hcode',
+                    email: 'contato@hcide.com.br',
+                    id: 1
+                }]
+            }))
+    
+        break
+
+    }
 
 })
 

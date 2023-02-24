f751a4d92a86c681bbd848fde6545ed030cb01e5
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-02-24 10:22:01 -0300

    feat: lessons 71, 72, 73 and 74: installing and configuring the nedb database, creating the utils folder for error formatting, fetching data with post request, fetching data with get request and fetching specific data with get and with the id parameter.

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,64 @@
+let NeDB = require('nedb')
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+})
+
 module.exports = (app) => {
 
-    app.get('/users', (req,res) => {
+    let route = app.route('/users')
 
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
-        res.json({
-            users: [{
-                name: 'Hcode',
-                email: 'contato@hcide.com.br',
-                id: 1
-            }]})
+    route.get((req,res) => {
+
+        db.find({}).sort({name: 1}).exec((err, users) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res)
+            } else {
+    
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json({
+                    users
+                })
+    
+            }
+    
+        })
     })
-    
-    app.post('/users', (req,res) => {
-    
-        
-        res.json(req.body)
+
+    route.post((req,res) => {
+
+        db.insert(req.body, (err, user) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res)
+            } else {
+
+                res.status(200).json(user)
+
+            }
+
+        })
     
     })
 
-}
+    let routeId = app.route('/users/:id')
+
+    routeId.get((req, res) => [
+
+        db.findOne({_id:req.params.id}).exec((err, user) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res)
+            } else {
+
+                res.status(200).json(user)
+                
+            }
+
+        })
+
+    ])
+
+} 
